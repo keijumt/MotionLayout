5c25e21f14e76a36214f3761148074bcc47e40cb
keijumt <keijumt@gmail.com>
2018-07-04 23:36:22 +0900

    Add basic motion

--- app/src/main/java/keijumt/motionlayout/ui/MainActivity.kt
+++ app/src/main/java/keijumt/motionlayout/ui/MainActivity.kt
@@ -1,13 +1,26 @@
 package keijumt.motionlayout.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import keijumt.motionlayout.Constant
 import keijumt.motionlayout.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_motion_01.setOnClickListener {
+            start(R.layout.motion_01)
+        }
+    }
+
+    private fun start(layoutId: Int) {
+        val intent = Intent(this, MotionActivity::class.java)
+        intent.putExtra(Constant.Arguments.layoutId.name, layoutId)
+        startActivity(intent)
     }
 }
